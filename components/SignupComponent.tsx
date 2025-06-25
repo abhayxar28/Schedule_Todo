39e09e7bf508b9174dddae5ed3a56876c37365d9
abhay <abhayarrawat28@gmail.com>
2025-06-26 01:49:20 +0530

    fixed backend route

--- components/SignupComponent.tsx
+++ components/SignupComponent.tsx
@@ -30,7 +30,7 @@ export default function SignupComponent() {
     setLoading(true);
 
     try {
-      const res = await fetch(`${process.env.BACKEND_URL}/signup`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/signup`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, name, password }),
