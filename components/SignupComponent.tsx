7f520fb5a305ec8993ca374db7a2e81ba9e63fec
abhay <abhayarrawat28@gmail.com>
2025-06-26 01:19:17 +0530

    fixed SignupComponent

--- components/SignupComponent.tsx
+++ components/SignupComponent.tsx
@@ -30,7 +30,7 @@ export default function SignupComponent() {
     setLoading(true);
 
     try {
-      const res = await fetch(`http://localhost:3000/api/signup`, {
+      const res = await fetch(`${process.env.BACKEND_URL}/signup`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, name, password }),
