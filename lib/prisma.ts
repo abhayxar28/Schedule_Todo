4efc4d34e3b9c34dc2d47ad24b64e4a559fc9673
abhay <abhayarrawat28@gmail.com>
2025-06-26 13:58:41 +0530

    prisma file change

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,6 +6,9 @@ const globalForPrisma = globalThis as unknown as {
 };
 
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient();
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error", "warn", "info"], 
+  });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
